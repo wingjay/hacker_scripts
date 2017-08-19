556d04b1f41ef14f248ef382139c4925e662a7c3
wingjay <yj142679@alibaba-inc.com>
2017-08-19 10:34:28 +0800

    自动从文件路径读取文件的title date permalink 参数

--- hexo_deploy.sh
+++ hexo_deploy.sh
@@ -1,26 +1,43 @@
 #!/bin/sh
-# 1. input must contains: title + date + permalink
+# 1. input article file path
 # 2. create a new issue, including article title and link
 # 3. deploy hexo
-if [ "$#" -ne 3 ]; then
-	echo "must input arguments: title date(2017-08-06) permalink"
+if [ "$#" -ne 1 ]; then
+	echo "必须输入文章路径"
 	exit 1
 fi
 
-# TODO: fetch title/date/permalink from .md file
-title=$1
-createDate=`echo $2 | tr '-' '/'`
-permalink=$3
+# Fetch title/date/permalink from .md file
+argument_count=0
+title=''
+date=''
+permalink=''
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    if [[ $line == title:*  ]]; then 
+        title=$(echo $line|cut -d" " -f2)
+        argument_count=$(($argument_count+1))
+    elif [[ $line == date:* ]]; then
+    	date=$(echo $line|cut -d" " -f2)
+    	argument_count=$(($argument_count+1))
+    elif [[ $line == permalink:* ]]; then
+    	permalink=$(echo $line|cut -d" " -f2)
+    	argument_count=$(($argument_count+1))
+    fi
+    if [[ $argument_count == 3 ]]; then
+    	break
+    fi  
+done < "$1"
+
+date=`echo $date | tr '-' '/'`
 
 # generate article link
-link='文章地址：http://wingjay.com/'$createDate'/'$permalink
-echo $link
-
-data='{"title":"《'$1'》评论","body":"'$link'"}'
+link='文章地址：http://wingjay.com/'$date'/'$permalink
+data='{"title":"《'$title'》评论","body":"'$link'"}'
+echo data: $data
 
 # create a issue with title + link
-commentId=$(curl --silent -H "Content-Type: application/json" -u wingjay:5871be02a817be2afd733ffbdbd6f5f8b1cb1289 -X POST -d $data https://api.github.com/repos/wingjay/hellojava/issues | jq -r '.number')
-echo $commentId
+commentId=$(curl --silent -H "Content-Type: application/json" -u wingjay:9d2ee1a50153311040db7f6d6ea8e46865d8b3d1 -X POST -d $data https://api.github.com/repos/wingjay/hellojava/issues | jq -r '.number')
+echo commentId: $commentId
 
 # replace commentId
 
